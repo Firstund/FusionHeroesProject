e28eeedfed4730313bac303f27110f6a10c535fe
Firstund <yjw040608@gmail.com>
2021-04-01 18:35:49 +0900

    메뉴창 나왔다 들어갔다 하게 만들기

--- Assets/UI/UIScript/M_ButtonScript.cs
+++ Assets/UI/UIScript/M_ButtonScript.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class M_ButtonScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private Transform buttonPosition = null;
+    private Vector2 menuButtonPositon = Vector2.zero;
+    [SerializeField]
+    private float speed = 0f;
+    [SerializeField]
+    private float firstSpeed = 0f;
 
-    // Update is called once per frame
+    [SerializeField]
+    private bool haveToBack = false;
+    [SerializeField]
+    private float comeBackVector = -2f;
+    [SerializeField]
+    private Vector2 currentPosition = Vector2.zero;
+    [SerializeField]
+    private MenuButtonScript menuButtonScript;
+    void Awake()
+    {
+        firstSpeed = speed;
+    }
     void Update()
     {
-        
+        currentPosition = transform.localPosition;
+
+        if (!haveToBack)
+            currentPosition = Vector2.Lerp(currentPosition, buttonPosition.localPosition, Time.deltaTime * speed); // 이동
+        else
+        {
+            currentPosition = Vector2.Lerp(currentPosition, menuButtonPositon, Time.deltaTime * speed); // 되돌아오기
+        }
+
+        if (currentPosition.x > comeBackVector && currentPosition.y > comeBackVector)
+        {
+            transform.localPosition = currentPosition;
+            speed = 0f;
+            menuButtonScript.IsComeBack();
+            Reset();
+            Return();
+        }
+
+        transform.localPosition = currentPosition;
+    }
+    public void Return()
+    {
+        haveToBack = !haveToBack;
+    }
+    public void Reset()
+    {
+        currentPosition = new Vector2(-2f, -2f);
+        transform.localPosition = currentPosition;
+    }
+    public void OnDisable()
+    {
+        speed = firstSpeed;
     }
 }
--- Assets/UI/UIScript/MenuButtonScript.cs
+++ Assets/UI/UIScript/MenuButtonScript.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class MenuButtonScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private bool menuOpen = false;
+    [SerializeField]
+    private int comeBackNum = 0;
 
-    // Update is called once per frame
-    void Update()
+    M_ButtonScript mBtnScript = null;
+    private void Update()
     {
-        
+        if (comeBackNum >= 3)
+        {
+            comeBackNum = 0;
+
+            MenuSet(false);
+        }
+    }
+    public void OnClick()
+    {
+        if (!menuOpen)
+        {    
+            MenuSet(true);
+        }
+        else
+        {
+            for (int i = 0; i < transform.GetChild(0).GetChildCount(); i++)
+            {
+                mBtnScript = transform.GetChild(0).GetChild(i).GetComponent<M_ButtonScript>();
+                mBtnScript.Return();
+            }
+        }
+        menuOpen = !menuOpen;
+    }
+    private void MenuSet(bool a)
+    {
+        transform.GetChild(0).gameObject.SetActive(a);
+    }
+    public void IsComeBack()
+    {
+        comeBackNum++;
     }
 }
+
