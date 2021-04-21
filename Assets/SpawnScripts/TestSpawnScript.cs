56b57366315beea931f7a3a8c47be93da1eb49fe
Firstund <yjw040608@gmail.com>
2021-04-22 00:44:21 +0900

    유닛소환 대기시간 구현

--- Assets/SpawnScripts/TestSpawnScript.cs
+++ Assets/SpawnScripts/TestSpawnScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TestSpawnScript : MonoBehaviour
 {
@@ -10,20 +11,52 @@ public class TestSpawnScript : MonoBehaviour
     private GameObject spawnThis = null;
     private Transform spawnPosition = null;
     [SerializeField]
+    private Image respawnImage = null;
+    [SerializeField]
     private int spawnMoney = 5;
+    [SerializeField]
+    private float spawnDelay = 3f;
+    private float nextTimeToSpawn = 0f;
+    private float respawnCurTime = 0f;
+    private float respawnMaxTime = 0f;
+    [SerializeField]
+    private bool canSpawnAgain = true;
     void Start()
     {
         gameManager = GameManager.Instance;
         spawnPosition = gameManager.GetUnitSpawnPosition();
     }
+    void Update()
+    {
+        checkTime();
+    }
    public void SpawnUnit()
     {
-        if (gameManager.GetMoney() >= spawnMoney)
+        if (gameManager.GetMoney() >= spawnMoney && canSpawnAgain)
         {
+            nextTimeToSpawn = Time.time + (1/spawnDelay);
             int money = gameManager.GetMoney() - spawnMoney;
             gameManager.SetMoney(money);
 
             Instantiate(spawnThis, spawnPosition);
+            respawnMaxTime = nextTimeToSpawn - Time.time;
+            respawnCurTime = 0f;
+            canSpawnAgain = false;
+        }
+    }
+    private void checkTime()
+    {
+        if(!canSpawnAgain)
+        {
+            respawnCurTime += Time.deltaTime;
+
+            respawnImage.fillAmount = respawnCurTime / respawnMaxTime;
+
+            if(respawnImage.fillAmount  >= 1f)
+            {
+                respawnImage.fillAmount = 1f;
+                canSpawnAgain = true;
+            }
         }
     }
 }
