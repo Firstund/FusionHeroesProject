a8f357956bb9d4a0f76505b27d53a722193a7d17
Firstund <yjw040608@gmail.com>
2021-05-19 00:56:04 +0900

    도감기능 사전준비

---
+++ Assets/UI/UIScript/EncyclopediaScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EncyclopediaScript : PopUpScaleScript
+{
+    void Start()
+    {
+        PlusStart();
+    }
+
+    void Update()
+    {
+        SetScale();
+    }
+}
