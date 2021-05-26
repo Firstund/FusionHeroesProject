797b4fcb5cc2601cd61e6c5e61ce52b5b72c2568
Firstund <yjw040608@gmail.com>
2021-05-26 18:58:36 +0900

    프로젝트 내의 모든 ViewPort 개선

---
+++ Assets/UI/UIScript/ScrollViewScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EncyclopediaScrollViewScript : MonoBehaviour
+{
+    private ScrollRect scrollRect = null;
+    private RectTransform rectTransform = null;
+    void Start()
+    {
+        scrollRect = GetComponent<ScrollRect>();
+
+        for (int i = 0; i < scrollRect.content.GetChildCount() - 1; i++)
+        {
+            scrollRect.content.sizeDelta += scrollRect.content.transform.GetChild(i).GetComponent<RectTransform>().sizeDelta;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
